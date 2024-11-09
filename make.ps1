484e5af8df25366f9e9c2c02f2286492038ff969
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-09 23:56:40 +0200

    fix make files

--- make.ps1
+++ make.ps1
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env pwsh
 ##############################################################################################################
 
-Function PrivClipper {
+Function Show-Usage {
     Return "
 Usage: pwsh -File $($PSCommandPath) [OPTIONS]
 Options:
@@ -9,56 +9,82 @@ Options:
 "
 }
 
-Function PrivPrepare {
-    $VAR = @(
-        @{
-            Cmd = 'lazbuild'
-            Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
-            Path = "C:\Lazarus"
+Function Request-File {
+    ForEach ($REPLY in $args) {
+        $params = @{
+            Uri = $REPLY
+            OutFile = (Split-Path -Path $REPLY -Leaf).Split('?')[0]
         }
-    )
-    ForEach ($REPLY in $VAR) {
-        If (-not (Get-Command $REPLY.Cmd -ea 'continue')) {
-            $params = @{
-                Uri = $REPLY.Url
-                OutFile = (Split-Path -Path $REPLY.Url -Leaf).Split('?')[0]
-            }
-            Invoke-WebRequest @params
-            Start-Process -PassThru -Wait -FilePath $params.OutFile -ArgumentList '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART'
-            Remove-Item $params.OutFile
-            $env:PATH+=";$($REPLY.Path)"
-            Get-Command $REPLY.Cmd
-        }
+        Invoke-WebRequest @params | Out-Null
+        Return $params.OutFile
     }
 }
 
-Function PrivMain {
+Function Install-Program {
+    While ($Input.MoveNext()) {
+        Switch ((Split-Path -Path $Input.Current -Leaf).Split('.')[-1]) {
+            'msi' {
+                & msiexec /passive /package $Input.Current | Out-Host
+            }
+            'exe' {
+                & ".\$($Input.Current)" /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART | Out-Host
+            }
+        }
+        Remove-Item $Input.Current
+    }
+}
+
+Function Build-Project {
+    $VAR = @{
+        Cmd = 'lazbuild'
+        Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
+        Path = "C:\Lazarus"
+    }
+    Try {
+        Get-Command $VAR.Cmd
+    } Catch {
+        Request-File $VAR.Url | Install-Program
+        $env:PATH+=";$($VAR.Path)"
+        Get-Command $VAR.Cmd
+    }
+    If ( Test-Path -Path 'use\components.txt' ) {
+        & git submodule update --recursive --init | Out-Host
+        & git submodule update --recursive --remote | Out-Host
+        Get-Content -Path 'use\components.txt' | ForEach-Object {
+            If ((-not (& lazbuild --verbose-pkgsearch $_ | Out-Null)) -and
+                (-not (& lazbuild --add-package $_ | Out-Null)) -and
+                (-not (Test-Path -Path 'use\components.txt'))) {
+                    $OutFile = Request-File "https://packages.lazarus-ide.org/$($_).zip"
+                    Expand-Archive -Path $OutFile -DestinationPath "use\$($_)" -Force
+                    Remove-Item $OutFile
+                }
+        }
+        Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
+            & lazbuild --add-package-link $_ | Out-Host
+        }
+    }
+    Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
+        & lazbuild --no-write-project --recursive --build-mode=release $_ | Out-Host
+    }
+}
+
+Function Switch-Action {
     $ErrorActionPreference = 'stop'
     Set-PSDebug -Strict -Trace 1
     Invoke-ScriptAnalyzer -EnableExit -Path $PSCommandPath
     If ($args.count -gt 0) {
         Switch ($args[0]) {
             'build' {
-                PrivPrepare
-                If (Test-Path -Path 'use') {
-                    Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--init'
-                    Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--remote'
-                    Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
-                        Start-Process -PassThru -Wait -FilePath 'lazbuild' -ArgumentList '--add-package-link', $_.Name
-                    }
-                }
-                Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
-                    Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--no-write-project', '--recursive', '--build-mode=release', $_.Name
-                }
+                Build-Project
             }
             Default {
-                PrivClipper
+                Show-Usage
             }
         }
     } Else {
-        PrivClipper
+        Show-Usage
     }
 }
 
 ##############################################################################################################
-PrivMain @args
+Switch-Action @args | Out-Null
